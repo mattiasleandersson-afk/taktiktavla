f83072c4b1622d7d2f79c7c4030d893412ac167e
mattiasleandersson-afk <mattias.leandersson@gmail.com>
2026-05-28 20:05:13 +0200

    Update app_test.js

--- app_test.js
+++ app_test.js
@@ -44840,7 +44840,7 @@ setTimeout(tt152RebindTaktikListButtons,1500);
 
 
 
-/* === v738 TEST: Taktiktavla fillista - ikoner närmare filnamn ===
+/* === v739 TEST: Taktiktavla fillista - ikoner närmare filnamn ===
    Bas: v722 TEST.
    Endast fillistans radbredd i Taktiktavla desktop.
    Syfte:
