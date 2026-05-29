a218824a7e8f3b35e5a7f9289c7cc7fe26f401d0
mattiasleandersson-afk <mattias.leandersson@gmail.com>
2026-05-29 08:28:59 +0200

    Update app_test.js

--- app_test.js
+++ app_test.js
@@ -1,5 +1,5 @@
 /* === v585 TESTMILJÖ FASTA FILNAMN ===
-   Klistra denna fil i GitHub som: app_test.js
+   Klistra denna fill i GitHub som: app_test.js
    Klistra indexfilen i GitHub som: index_test.html
    Index laddar app_test.js?v=585-test och ska inte peka på produktionens app.js.
 */
